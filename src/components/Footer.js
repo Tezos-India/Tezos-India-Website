e3a1baaf96be364e1aba7a61426bba1ab7e2ac7b
Tejaaswini <tejnaren07@gmail.com>
2020-11-30 16:10:33 +0530

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,52 +8,6 @@ import twitter from "../assets/Twitter.svg";
 import youtube from "../assets/YouTube.svg";
 
 function Footer() {
-  //   return (
-  //     <div className={footerStyles.container}>
-  //       <div className={footerStyles.footerContent}>
-  //         <div className={footerStyles.logoContainer}>
-  //           <img src={logo} alt="" className={footerStyles.logo} />
-  //         </div>
-  //         <div className={footerStyles.footerLinks}>
-  //           <p className={footerStyles.footerAlignLinks}>Home</p>
-  //           <p className={footerStyles.footerAlignLinks}>Pages</p>
-  //           <p className={footerStyles.footerAlignLinks}>Blog</p>
-  //           <p className={footerStyles.footerAlignLinks}>Portfolio</p>
-  //           <p className={footerStyles.footerAlignLinks}>Contact</p>
-  //         </div>
-  //         <div className={footerStyles.social}>
-  //           <img
-  //             src={dribbble}
-  //             alt=""
-  //             className={footerStyles.footerSocialLinks}
-  //           />
-  //           <img
-  //             src={facebook}
-  //             alt=""
-  //             className={footerStyles.footerSocialLinks}
-  //             style={{ width: "12px" }}
-  //           />
-  //           <img
-  //             src={twitter}
-  //             alt=""
-  //             className={footerStyles.footerSocialLinks}
-  //           />
-  //           <img
-  //             src={youtube}
-  //             alt=""
-  //             className={footerStyles.footerSocialLinks}
-  //           />
-  //         </div>
-  //       </div>
-  //       <div  className={footerStyles.copyright}>
-  //       <p>
-  //         © Copyright 2019 Tezos India Foundation - All Rights Reserved{" "}
-  //       </p>
-  //       </div>
-  //     </div>
-  //   );
-  // }
-
   return (
     <div className={footerStyles.container}>
       <Grid container>
