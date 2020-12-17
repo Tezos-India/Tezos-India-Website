4bf1ae5bf7415c946ed74ce17b512c253fd71bc7
Tejaaswini <tejnaren07@gmail.com>
2020-12-17 18:24:42 +0530

    update team section

--- src/components/Team.js
+++ src/components/Team.js
@@ -14,49 +14,48 @@ export default function Team() {
       }
     >
       <h1 className="team-heading">The A Team</h1>
+      <div></div>
       <div className="team-align-cards">
-      <div className="team-align--horizontal">
-        {team.teammates.map((member) => (
-          <Card>
-            <div className="team-alignImage">
-              <img src={member.image} alt="" className="team-memberImg" />
-              <p className="team-header">{member.name}</p>
-              <p className="team-designation">{member.designation}</p>
-              <div className="teamSocial">
-                <img src={member.instagram} alt="" />
-                <img src={member.dribbble} alt="" />
-                <img src={member.linkedin} alt="" />
+        <div className="team-align--horizontal">
+          {team.teammates.map((member) => (
+            <Card>
+              <div className="team-alignImage">
+                <img src={member.image} alt="" className="team-memberImg" />
+                <p className="team-header">{member.name}</p>
+                <p className="team-designation">{member.designation}</p>
+                <div className="teamSocial">
+                  <img src={member.instagram} alt="" />
+                  <img src={member.dribbble} alt="" />
+                  <img src={member.linkedin} alt="" />
+                </div>
               </div>
-            </div>
-          </Card>
-        ))}
-      </div>
-      <div className="team-align--horizontal">
-        {team.teamamtesRowTwo.map((member) => (
-          <Card>
-            <div className="team-alignImage">
-              <img src={member.image} alt="" className="team-memberImg" />
-              <p className="team-header">{member.name}</p>
-              <p className="team-designation">{member.designation}</p>
-              <div className="teamSocial">
-                <img src={member.instagram} alt="" />
-                <img src={member.dribbble} alt="" />
-                <img src={member.linkedin} alt="" />
+            </Card>
+          ))}
+        </div>
+        <div className="team-align--horizontal">
+          {team.teamamtesRowTwo.map((member) => (
+            <Card>
+              <div className="team-alignImage">
+                <img src={member.image} alt="" className="team-memberImg" />
+                <p className="team-header">{member.name}</p>
+                <p className="team-designation">{member.designation}</p>
+                <div className="teamSocial">
+                  <img src={member.instagram} alt="" />
+                  <img src={member.dribbble} alt="" />
+                  <img src={member.linkedin} alt="" />
+                </div>
               </div>
-            </div>
-          </Card>
-        ))}
-      </div>
+            </Card>
+          ))}
+        </div>
       </div>
       <div>
-      <Button
-        className={
-          isDark ? "teamMore-btn dark-teamMore-btn" : "teamMore-btn"
-        }
-      >
-        Still have unanswered questions? Get in touch
-      </Button>
-        </div>
+        <Button
+          className={isDark ? "teamMore-btn dark-teamMore-btn" : "teamMore-btn"}
+        >
+          Still have unanswered questions? Get in touch
+        </Button>
+      </div>
     </div>
   );
 }
